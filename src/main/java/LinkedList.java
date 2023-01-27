4f46c5b765b9b6729dd87d68a7c7c709369b2db4
Mason Wudtee <mason.wudtee@gmail.com>
2023-01-26 21:12:53 -0600

    first exercise for datastructures udemy course complete

---
+++ src/main/java/LinkedList.java
@@ -0,0 +1,48 @@
+public class LinkedList {
+    //// Instance Variables
+    private int value;
+    private Node head;
+    private Node tail;
+    private int length;
+
+    // CREATE CLASS VARIABLES, NODE CLASS, AND CONSTRUCTOR HERE //
+    public LinkedList(){}
+    public LinkedList(int value){
+//        this.value = value;
+        head = new Node(value);
+        tail = new Node(value);
+        length = 1;
+    }
+
+
+    //////////////////////////////////////////////////////////////
+
+
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void getHead() {
+        if (head == null) {
+            System.out.println("Head: null");
+        } else {
+            System.out.println("Head: " + head.value);
+        }
+    }
+
+    public void getTail() {
+        if (head == null) {
+            System.out.println("Tail: null");
+        } else {
+            System.out.println("Tail: " + tail.value);
+        }
+    }
+
+    public void getLength() {
+        System.out.println("Length: " + length);
+    }
+}
---
+++ src/main/java/Node.java
@@ -0,0 +1,17 @@
+public class Node {
+    public int value;
+    public Node next;
+
+    //// Constructor
+    public Node(){}
+
+    public Node(int value){
+        this.value = value;
+    }
+
+    //// Getters and setters
+
+    public int getValue(){
+        return this.value;
+    }
+}
