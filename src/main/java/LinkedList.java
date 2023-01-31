9be84edf7e295ba41bbf1364eae461c2a4b907bc
Mason Wudtee <mason.wudtee@gmail.com>
2023-01-30 19:08:52 -0600

    prepend

--- src/main/java/LinkedList.java
+++ src/main/java/LinkedList.java
@@ -108,6 +108,17 @@ public class LinkedList {
 
     }
 
+    public void prepend(int value){
+        Node newNode = new Node(value);
+        if (length == 0) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode.next = head;
+            head = newNode;
+        }
+        length++;
+    }
 
 
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,6 +6,20 @@ public class Main {
 
     public static void main(String[] args) {
 
+        LinkedList prependLinkedList = new LinkedList(2);
+        prependLinkedList.append(3);
+        prependLinkedList.append(3);
+        prependLinkedList.append(5);
+        prependLinkedList.printList();
+        System.out.println();
+        prependLinkedList.prepend(1);
+        prependLinkedList.printList();
+        prependLinkedList.removeLast();
+
+        System.out.println("remove last");
+
+        prependLinkedList.printList();
+
 //        myLinkedList.getHead();
 //        myLinkedList.getTail();
 //        myLinkedList.getLength();
@@ -24,32 +38,32 @@ public class Main {
             4
 
         */
-        LinkedList myLinkedList = new LinkedList(4);
-        LinkedList newLinkedList = new LinkedList(4);
-        newLinkedList.append(2);
-        myLinkedList.append(7);
-        myLinkedList.append(14);
-        myLinkedList.append(24);
-        myLinkedList.append(10);
-
-        myLinkedList.getHead();
-        myLinkedList.getTail();
-        myLinkedList.getLength();
-
-        myLinkedList.printList();
-
-        System.out.println();
-//        System.out.println(myLinkedList.removeLast());
-        myLinkedList.getHead();
-        myLinkedList.getTail();
-        myLinkedList.getLength();
-        System.out.println();
-        myLinkedList.printList();
-        System.out.println();
-
-        System.out.println(newLinkedList.removeLast().value);
-        System.out.println(newLinkedList.removeLast().value);
+//        LinkedList myLinkedList = new LinkedList(4);
+//        LinkedList newLinkedList = new LinkedList(4);
+//        newLinkedList.append(2);
+//        myLinkedList.append(7);
+//        myLinkedList.append(14);
+//        myLinkedList.append(24);
+//        myLinkedList.append(10);
+//
+//        myLinkedList.getHead();
+//        myLinkedList.getTail();
+//        myLinkedList.getLength();
+//
+//        myLinkedList.printList();
+//
+//        System.out.println();
+////        System.out.println(myLinkedList.removeLast());
+//        myLinkedList.getHead();
+//        myLinkedList.getTail();
+//        myLinkedList.getLength();
+//        System.out.println();
+//        myLinkedList.printList();
+//        System.out.println();
+//
 //        System.out.println(newLinkedList.removeLast().value);
+//        System.out.println(newLinkedList.removeLast().value);
+////        System.out.println(newLinkedList.removeLast().value);
 
     }
 
