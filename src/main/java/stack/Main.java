4983d7947f7866e875c827c57c160ff622cc3d0d
Mason Wudtee <mason.wudtee@gmail.com>
2023-01-31 20:44:18 -0600

    Stack Push Complete

--- src/main/java/stack/Main.java
+++ src/main/java/stack/Main.java
@@ -3,11 +3,14 @@ package stack;
 public class Main {
 
     public static void main(String[] args) {
-        Stack myStack = new Stack(4);
+        Stack myStack = new Stack(2);
+        // myStack.getTop();
+        // myStack.getHeight();
 
-        myStack.getTop();
-        myStack.getHeight();
+        myStack.printStack();
 
+        System.out.println("Push a new stack item");
+        myStack.push(1);
         myStack.printStack();
     }
 }
