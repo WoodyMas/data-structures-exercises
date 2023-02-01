7ae05d6d8de0169681ab39b4a7028e7f1ede4776
Mason Wudtee <mason.wudtee@gmail.com>
2023-01-31 20:48:32 -0600

    Stack Pop

--- src/main/java/stack/Main.java
+++ src/main/java/stack/Main.java
@@ -12,5 +12,11 @@ public class Main {
         System.out.println("Push a new stack item");
         myStack.push(1);
         myStack.printStack();
+
+        System.out.println("Test myStack.pop()");
+        System.out.println(myStack.pop().value);
+        System.out.println("It returned the first index as desired, but let's see if myStack actually has lost the first index");
+        myStack.printStack();
+        System.out.println("It works");
     }
 }
