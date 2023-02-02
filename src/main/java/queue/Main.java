f18af1b23257fb79e5d03f4f3c37d74c23a985a8
Mason Wudtee <mason.wudtee@gmail.com>
2023-02-01 23:39:21 -0600

    Queue Enqueue

--- src/main/java/queue/Main.java
+++ src/main/java/queue/Main.java
@@ -4,11 +4,26 @@ public class Main {
 
     public static void main(String[] args) {
         Queue myQueue = new Queue(7);
+        Queue nullQueue = new Queue();
 
         myQueue.getFirst();
         myQueue.getLast();
         myQueue.getLength();
 
         myQueue.printQueue();
+
+        System.out.println();
+
+        myQueue.enqueue(5);
+        myQueue.enqueue(    15);
+        myQueue.printQueue();
+
+
+        System.out.println();
+        nullQueue.printQueue();
+        nullQueue.getLength();
+        System.out.println();
+        nullQueue.enqueue(5);
+        nullQueue.printQueue();
     }
 }
--- src/main/java/queue/Queue.java
+++ src/main/java/queue/Queue.java
@@ -21,7 +21,10 @@ public class Queue {
         length =1;
     }
 
+    public Queue() {}
+
     public void printQueue() {
+        if (length == 0) System.out.println("Queue empty");
         Node temp = first;
         while (temp != null) {
             System.out.println(temp.value);
@@ -29,15 +32,30 @@ public class Queue {
         }
     }
 
+    public void enqueue(int value){
+        Node newNode = new Node(value);
+        if (length == 0){
+            first = newNode;
+            last = newNode;
+        } else {
+            last.next = newNode;
+            last = newNode;
+        }
+        length++;
+    }
+
+
+
     public void getFirst() {
         System.out.println("First: " + first.value);
     }
 
     public void getLast(){
-        System.out.println("LAst: " + last.value);
+        System.out.println("Last: " + last.value);
     }
 
     public void getLength() {
+        if (length == 0) System.out.println("Queue is empty: no length to return");
         System.out.println("Length: " + length);
     }
 }
