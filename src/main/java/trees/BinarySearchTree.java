ce1bf9609656e67bb285efa99b4dd247a0b5c758
Mason Wudtee <mason.wudtee@gmail.com>
2023-02-06 21:47:34 -0600

    rBST Insert code complete

--- src/main/java/trees/BinarySearchTree.java
+++ src/main/java/trees/BinarySearchTree.java
@@ -112,6 +112,44 @@ public class BinarySearchTree {
         return false;
     }
 
+    private Node rInsert (Node currentNode, int value) {
+        if (currentNode == null) return new Node(value);
+
+        if (value < currentNode.value) {
+            currentNode.left = rInsert(currentNode.left, value);
+        } else if (value > currentNode.value) {
+            currentNode.right = rInsert(currentNode.right, value);
+        }
+        return currentNode;
+        }
+
+
+    public void  rInsert(int value) {
+        if (root == null) root = new Node(value);
+        rInsert(root, value);
+    }
+
+
+
+    public boolean rContains(int value) {
+        return rContains(root, value);
+    }
+
+    private boolean rContains(BinarySearchTree.Node currentNode, int value) {
+        if (currentNode == null) return false;
+        if (currentNode.value == value) return true;
+
+        if (value < currentNode.value) {
+            return rContains(currentNode.left, value);
+        } else {
+            return rContains(currentNode.right, value);
+        }
+
+
+    }
+
+
+
     public static void main(String[] args) {
         System.out.println("""
                 //////////////////////////////////////////////////////////////////
--- src/main/java/trees/Main.java
+++ src/main/java/trees/Main.java
@@ -3,6 +3,7 @@ package trees;
 public class Main {
     public static void main(String[] args) {
         BinarySearchTree myBST = new BinarySearchTree();
+//        RecursiveBinarySearchTree myRBST = new RecursiveBinarySearchTree();
         System.out.println("Root = " + myBST.root);
 
         myBST.insert(45);
@@ -23,9 +24,28 @@ public class Main {
         System.out.println(myBST.contains(45));
         System.out.println(myBST.contains(48));
         System.out.println(myBST.contains(32));
+        System.out.println("Test false?");
         System.out.println(myBST.contains(33));
 
 
+        System.out.println("\nTesting Recursive on myBST");
+        System.out.println(myBST.rContains(32));
+        System.out.println(myBST.rContains(70));
+        System.out.println(myBST.rContains(33));
+
+
+        BinarySearchTree myRBST = new BinarySearchTree();
+
+        System.out.println("Test myRBST contains when null");
+        System.out.println(myRBST.rContains(34));
+        myRBST.rInsert(2);
+        myRBST.rInsert(1);
+        myRBST.rInsert(3);
+
+        System.out.println(myRBST.root.value);
+        System.out.println(myRBST.root.left.value);
+        System.out.println(myRBST.root.right.value);
+//        System.out.println(myRBST.root.right.value);
 
 
     } // end main method
